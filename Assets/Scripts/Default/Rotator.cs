b8df87f4f96d11047eb062ed6c99cae05b3c6517
Davide Aversa <thek3nger@gmail.com>
2019-09-29 14:50:02 +0200

    First Commit

---
+++ Assets/Scripts/Default/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Classic
+{
+	public class Rotator : MonoBehaviour
+	{
+
+		public float speed;
+
+		// Update is called once per frame
+		void Update()
+		{
+			transform.Rotate(0f, speed * Time.deltaTime, 0f);
+		}
+	}
+}
---
+++ Assets/Scripts/ECSJob/RotationSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Burst;
+
+namespace ECSJob
+{
+    public class RotationSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        public struct RotatorJob : IJobForEach<Rotation, RotationSpeed>
+        {
+
+            [ReadOnly]
+            public float deltaTime;
+
+            public void Execute(ref Rotation rotation, [ReadOnly] ref RotationSpeed rotationSpeed)
+            {
+                //Debug.Log("Prima " + quaternion. rotation.Value .ToString());
+                //Debug.Log("Speed " + rotationSpeed.Value);
+                rotation.Value = math.mul(math.normalize(rotation.Value), quaternion.AxisAngle(math.up(), rotationSpeed.Value * deltaTime));
+                //Debug.Log("Dopo " + rotation.Value.ToString());
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            RotatorJob rotatorJob = new RotatorJob()
+            {
+                deltaTime = Time.deltaTime
+            };
+
+            return rotatorJob.Schedule(this, inputDeps);
+        }
+    }
+}
